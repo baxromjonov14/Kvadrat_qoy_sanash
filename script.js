45daafa44240067e93fab9d00ee600e06f533105
baxromjonov14@gmail.com <Baxromjonov14@gmail.com>
2025-03-06 14:35:03 +0500

    kvadrat

---
+++ script.js
@@ -0,0 +1,44 @@
+let num = +prompt("Qo'ylar sonini kiriting.")
+while (isNaN(num) || num==0) {
+    num = +prompt("Qo'ylar sonini kiriting.")
+}
+
+let qoyxona = ""
+
+for (let i = 0; i < num; i++) {
+    if (i+1==1) {
+        qoyxona += `${i+1}ta qo'y... `
+    } else {
+        qoyxona += `${i+1}ta qo'ylar... `
+    }
+}
+console.log(qoyxona);
+
+
+
+
+
+
+
+
+
+let son = +prompt("Kvadrat tomoni uzunligini kiriting.")
+while (isNaN(son) || son==0) {
+    son = +prompt("Kvadrat tomoni uzunligini kiriting.")
+}
+alert("Natijani ko'rish uchun Console oynasiga o'ting.")
+let kvadrat = ""
+for (let i = 0; i < son; i++) {
+    for (let j = 0; j < son; j++) {
+        if (i==j || i==0 || j==0 || i==son-1 || j==son-1) {
+            kvadrat += "{*}"
+        } else {
+            kvadrat += "   "
+        }
+    }
+    console.log(kvadrat);
+    kvadrat = ''
+}
+
+
+
